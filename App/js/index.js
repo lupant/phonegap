c9c8ae5f184185eab5d9b5b91ddf24e2eaf0ef85
lupant <lupant@gmail.com>
2016-02-12 06:51:06 +0100

    dock width

--- App/js/index.js
+++ App/js/index.js
@@ -18,8 +18,10 @@ Class('App', 'xui.Com',{
             append((new xui.UI.Div())
             .setHost(host,"ctl_div7")
             .setDomId("mapg")
-            .setDock("fill")
+            .setDock("width")
             .setDockMargin({"left":30, "top":120, "right":30, "bottom":30})
+            .setTop(120)
+            .setHeight(318)
             .setCustomStyle({"KEY":{"border":"solid #FF8C00 1px", "box-shadow":"6px 6px 10px #F4A460", "$gradients":""}})
             );
             
